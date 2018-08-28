8e97c9b91e17511f1201050afe85b9636661978f
carlos andres gaviria rebolledo <costa.gaviria@gmail.com>
2018-08-27 19:50:33 -0500

    Add files via upload

---
+++ Intefaz_carlos gaviria/src/intefaz/Cantante.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package intefaz;
+
+/**
+ *
+ * @author ESTUDIANTE
+ */
+public class Cantante implements Voz{
+    public Cantante(){
+    
+    }
+
+    
+    public void cantar() {
+        System.out.println("Pio pio");
+    }
+
+    
+    public void hablar() {
+       System.out.println("lala lala lalal");
+    }
+    
+}
---
+++ Intefaz_carlos gaviria/src/intefaz/Intefaz.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package intefaz;
+
+/**
+ *
+ * @author ESTUDIANTE
+ */
+public class Intefaz {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        Cantante c = new Cantante();
+        
+        c.cantar();
+    }
+    
+}
