8b34d1070fb2368585d6ecbc8efcee1a99ee6eab
Oscar Andres Hernanez Ariza <ohernandezar@uninpahu.edu.co>
2018-09-20 21:59:23 -0500

    Figonacci Correccion de menu 3 intentos

--- Oscar Andres Hernandez Ariza/Figonachi/src/edu/co/uninpahu/figonachi/Main.java
+++ Oscar Andres Hernandez Ariza/Figonachi/src/edu/co/uninpahu/figonachi/Main.java
@@ -20,25 +20,36 @@ public class Main {
         Long f[] = {0L, 1L};// matiz inicial
         int cantidad = 0;
         Long total = 0L;
+        short intentos = 0;
+        boolean complete = true;
 
-        System.out.print("cuantos numeros desea :");
-        Scanner teclado = new Scanner(System.in);
-        try {
-            cantidad = teclado.nextInt();//lee la cantidad de series que se quiere calcular
-            if (cantidad > 92) {// si se calculan mas de 92 
-                System.out.println("no se puede calcular mas de 92 numeros");
-            } else {
-                for (int i = 0; i < cantidad; i++) {//calcula e imprime la serie
-                    System.out.print(" " + f[0]);
-                    total = (f[1] + f[0]);
-                    f[0] = f[1];
-                    f[1] = total;
+        Scanner teclado;
+        do {
+            System.out.print("cuantos numeros desea :");
+            try {
+                teclado = new Scanner(System.in);
+                cantidad = teclado.nextInt();//lee la cantidad de series que se quiere calcular
+                if (cantidad > 92 || cantidad < 0) {// si se calculan mas de 92 
+                    System.out.println("No se puede calcular mas de 92 numeros ni cantidades negativos");
+                } else {
+                    for (int i = 0; i < cantidad; i++) {//calcula e imprime la serie
+                        System.out.print(" " + f[0]);
+                        total = (f[1] + f[0]);
+                        f[0] = f[1];
+                        f[1] = total;
+                    }
+                    System.out.println("");//salto de linea final
+                    complete = false;
                 }
-                System.out.println("");//salto de linea final
+            } catch (java.util.InputMismatchException e) {//controla si se ingresa alguna letra en la lectura
+                if (!(intentos < 2)) {
+                    System.err.println("Se a exedido el maximo de intentos");
+                } else {
+                    System.out.println("Por favor ingrese un numero entero");
+                }
             }
-        } catch (java.util.InputMismatchException e) {//controla si se ingresa alguna letra en la lectura
-            System.out.println("por favor ingrese un numero entero");
-        }
+            intentos++;
+        } while (intentos < 3 && complete);
 
     }
 
