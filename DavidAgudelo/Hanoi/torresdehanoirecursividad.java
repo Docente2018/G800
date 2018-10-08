5d88b0a4872105cc37872ee963ba280ad203a18e
fagudeloor <42949960+fagudeloor@users.noreply.github.com>
2018-10-08 18:24:05 -0500

    Update torresdehanoirecursividad.java

--- DavidAgudelo/Hanoi/torresdehanoirecursividad.java
+++ DavidAgudelo/Hanoi/torresdehanoirecursividad.java
@@ -15,14 +15,20 @@ public class TorresDeHanoiRecursividad {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        int cantidadDiscos = 5;
+        int cantidadDiscos = 5;//numero de discos a ordenar
         ordenarTorres(cantidadDiscos, 'A', 'B', 'C');
     }
-    
+    /**
+    *metodo recursivo que ordena las torres
+    *@param int topN: disco superior
+    *@param char origen: torre origen
+    *@param char inter: torre intermedia
+    *@param char to: torre destino
+    **/
       public static void ordenarTorres(int topN, char origen, char inter, char to) {
-        if (topN == 1){
+        if (topN == 1){// si es el ultimo disco
             System.out.println("Disco 1 desde " + origen + " a " + to);
-        }else {
+        }else {//si no es el ultimo ordenar disco y volver a llamarse
             ordenarTorres(topN - 1, origen, to, inter);
             System.out.println("Disco " + topN + " desde " + origen + " hacia " + to);
             // llamada recursiva
