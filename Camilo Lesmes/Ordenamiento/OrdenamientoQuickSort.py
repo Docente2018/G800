a300a9ce86343f72f71253ecd8c67abfa5d35f0a
Camilo Lesmes <13592195+CamiloVeloz@users.noreply.github.com>
2018-10-18 20:49:32 -0500

    Update OrdenamientoQuickSort.py

--- Camilo Lesmes/Ordenamiento/OrdenamientoQuickSort.py
+++ Camilo Lesmes/Ordenamiento/OrdenamientoQuickSort.py
@@ -1,48 +1,37 @@
-# -*- coding:utf-8 -*-
-def quick_sort(A):
-    quick_sort2(A, 0, len(A)-1)
-    
-def quick_sort2(A, bajo, alto):
-    if alto-bajo < threshold and bajo < alto:
-        quick_selection(A, bajo, alto)
-    elif bajo < alto:
-        p = partition(A, bajo, alto)
-        quick_sort2(A, bajo, p - 1)
-        quick_sort2(A, p + 1, alto)
-    
-def obternerPivote(A, bajo, alto):
-    mid = (alto + bajo) // 2
-    s = sorted([A[bajo], A[mid], A[alto]])
-    if s[1] == A[bajo]:
-        return bajo
-    elif s[1] == A[mid]:
-        return mid
-    return alto
-    
-def partition(A, bajo, alto):
-    pivotIndex = obternerPivote(A, bajo, alto)
-    ValorPivote = A[pivotIndex]
-    A[pivotIndex], A[bajo] = A[bajo], A[pivotIndex]
-    border = bajo
+# -*- coding: utf-8 -*-
 
-    for i in range(bajo, alto+1):
-        if A[i] < ValorPivote:
-            border += 1
-            A[i], A[border] = A[border], A[i]
-    A[bajo], A[border] = A[border], A[bajo]
+from time import time
 
-    return (border)
-    
-def quick_selection(x, primero, final):
-    for i in range (primero, final):
-        minIndex = i
-        for j in range (i+1, final+1):
-            if x[j] < x[minIndex]:
-                minIndex = j
-        if minIndex != i:
-            x[i], x[minIndex] = x[minIndex], x[i]
-            
-A = [5,9,1,2,4,8,6,3,7]
-print(A)
-quick_sort(A)
-print(A)
+def particion(lista, izq, der):
+    global comparaciones
+    pivote = lista[der]
+    indice = izq
+
+    for i in xrange(izq, der):
+        comparaciones += 1
+        
+        if lista[i] <= pivote:
+            lista[indice], lista[i] = lista[i], lista[indice]
+            indice += 1
+
+    lista[indice], lista[der] = lista[der], lista[indice]
+    return indice
+
+def quicksort(lista, izq, der):
+    if izq < der:
+        pivote_indice = particion(lista, izq, der)
+        quicksort(lista, izq, pivote_indice-1)
+        quicksort(lista, pivote_indice+1, der)
+
+
+lista = [36, 71, 16, 21, 73, 9, 0, 40, 66, 5]
+comparaciones = 0
+
+t0 = time()
+quicksort(lista, 0, len(lista)-1)
+t1 = time()
+
+print "Lista ordenada:"
+print lista, "\n"
+
+print "Comparaciones:", comparaciones
