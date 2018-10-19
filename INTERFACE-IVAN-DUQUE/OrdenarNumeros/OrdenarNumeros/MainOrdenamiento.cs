1b67c35c370ee108d280bb570dd96489246a94bc
davidduque29 <ivduque@hotmail.com>
2018-10-18 20:32:13 -0500

    Add files via upload

---
+++ INTERFACE-IVAN-DUQUE/OrdenarNumeros/OrdenarNumeros/MainOrdenamiento.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OrdenarNumeros
+{
+    class MainOrdenamiento
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("::: METODOS DE ORDENAMIENTO :::\n");
+
+            int opcion = 0;
+            try
+            {
+                Console.WriteLine("Digite el numero de la opcion:\n");
+                Console.WriteLine(" - 1.METODO BURBUJA\n - 2.METODO QUICK SORT\n - 3.METODO SHELL SORT\n - 4.METODO MERGE SORT\n");
+                opcion = Convert.ToInt32(Console.ReadLine());
+
+                switch (opcion)
+                {
+                    case 1: ;
+                        Console.WriteLine("\n___________________________\n");
+                        Console.WriteLine("::: 1.METODO BURBUJA :::");
+                        Console.WriteLine("\n___________________________\n");
+
+                        MetodoBurbuja mb = new MetodoBurbuja();
+                        mb.cargar();
+                        mb.ProcMetodoBurbuja();
+                        mb.imprimir();
+
+                        Console.Read();
+                        break;
+                    case 2: ;
+                        Console.WriteLine("\n___________________________\n");
+                        Console.WriteLine("::: 2.METODO QUICK SORT:::");
+                        Console.WriteLine("\n___________________________\n");
+
+                        MetodoQuickSort mqs = new MetodoQuickSort();
+                        mqs.cargar();
+
+                        break;
+                    case 3: ;
+                        Console.WriteLine("\n___________________________\n");
+                        Console.WriteLine("::: 3.METODO SHELL SORT :::");
+                        Console.WriteLine("\n___________________________\n");
+
+                        MetodoShellSort mss = new MetodoShellSort();
+                        mss.cargar();
+                        mss.Shell();
+                        mss.Imprimir();
+
+                        break;
+                    case 4: ;
+                        Console.WriteLine("\n___________________________\n");
+                        Console.WriteLine("::: 4.METODO MERGE SORT :::");
+                        Console.WriteLine("\n___________________________\n");
+
+                        MetodoMergeSort mms = new MetodoMergeSort();
+                        mms.cargar();
+                      
+
+                        break;
+                    default:
+                        Console.WriteLine("ERROR");
+                        break;
+                }
+            }
+            catch (Exception)
+            {
+                Console.WriteLine("Error solo numeros");
+                Console.ReadLine();
+             
+            }
+           
+
+        }
+    }
+}
---
+++ INTERFACE-IVAN-DUQUE/OrdenarNumeros/OrdenarNumeros/MetodoMergeSort.cs
@@ -0,0 +1,131 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OrdenarNumeros
+{
+    class MetodoMergeSort
+    {
+        string linea;
+        int[] nums = new int[40];
+        //Se debe crear un metodo tipo void para poder usar el Console.Writeline
+        public void cargar()
+        {
+            int cont = 0;
+            for (int i = 0; i < 3; i++)
+            {
+                try
+                {
+                   
+                    Console.WriteLine("\n::::ORDENAMIENTO DE NUMEROS(Metodo Merge Sort)::::\n");
+                    Console.Write("\n-Ingrese de cuanto sera la longitud del vector: \n");
+                    linea = Console.ReadLine();
+
+                    if (cont == 0)
+                    {
+                        break;
+                    }
+                    else if (cont == 3)
+                    {
+                        Console.WriteLine("::: Intentos permitidos superados :::");
+                        Console.Read();
+                    }
+                }
+                catch (Exception)
+                {
+                    cont++;
+                    Console.WriteLine("\n----------------------\n");
+                    Console.WriteLine("¡Error, solo numeros!\nintente de nuevo.");
+                    Console.WriteLine("Intento# " + cont + "\n----------------------\n");
+                }
+
+            }
+            int cant;
+            cant = int.Parse(linea);
+            nums = new int[cant];
+            for (int f = 0; f < nums.Length; f++)
+            {
+                Console.Write("Ingrese elemento " + (f + 1) + ": ");
+                linea = Console.ReadLine();
+                nums[f] = int.Parse(linea);
+            }
+            ProcMetodoMergeSort(nums);
+            Console.WriteLine("Vector Ordenado Ascendentemente");
+            for (int i = 0; i < nums.Length; i++)
+                Console.Write(nums[i] + " ");
+            Console.ReadLine();
+
+        } 
+        //Método portal que llama al método recursivo inicial
+        public static void ProcMetodoMergeSort(int[] x)
+        {
+            ProcMetodoMergeSort(x, 0, x.Length - 1);
+        }
+
+        static private void ProcMetodoMergeSort(int[] x, int desde, int hasta)
+        {
+            //Condicion de parada
+            if (desde == hasta)
+                return;
+            //Calculo la mitad del array
+            int mitad = (desde + hasta) / 2;
+            //Voy a ordenar recursivamente la primera mitad
+            //y luego la segunda
+            ProcMetodoMergeSort(x, desde, mitad);
+            ProcMetodoMergeSort(x, mitad + 1, hasta);
+            //Mezclo las dos mitades ordenadas
+            int[] aux = Merge(x, desde, mitad, mitad + 1, hasta);
+            Array.Copy(aux, 0, x, desde, aux.Length);
+        }
+
+        //Método que mezcla las dos mitades ordenadas
+        static private int[] Merge(int[] x, int desde1, int hasta1, int desde2, int hasta2)
+        {
+            int a = desde1;
+            int b = desde2;
+            int[] result = new int[hasta1 - desde1 + hasta2 - desde2 + 2];
+
+            for (int i = 0; i < result.Length; i++)
+            {
+                if (b != x.Length)
+                {
+                    if (a > hasta1 && b <= hasta2)
+                    {
+                        result[i] = x[b];
+                        b++;
+                    }
+                    if (b > hasta2 && a <= hasta1)
+                    {
+                        result[i] = x[a];
+                        a++;
+                    }
+                    if (a <= hasta1 && b <= hasta2)
+                    {
+                        if (x[b] <= x[a])
+                        {
+                            result[i] = x[b];
+                            b++;
+                        }
+                        else
+                        {
+                            result[i] = x[a];
+                            a++;
+                        }
+                    }
+                }
+                else
+                {
+                    if (a <= hasta1)
+                    {
+                        result[i] = x[a];
+                        a++;
+                    }
+                }
+            }
+            return result;
+        }
+    }
+}
+
