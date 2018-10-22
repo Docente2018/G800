abcb8c1a521c01e4151c140f5d6269370be52b79
fagudeloor <42949960+fagudeloor@users.noreply.github.com>
2018-10-22 18:53:05 -0500

    Update Insertion.java

--- DavidAgudelo/sortmetods/Insertion.java
+++ DavidAgudelo/sortmetods/Insertion.java
@@ -9,15 +9,36 @@ package sortmetods;
  *
  * @author ESTUDIANTES
  */
+
 public class Insertion {
-    public static void Insercion (int[] vector) {
-      for (int i=1; i < vector.length; i++) {
-         int aux = vector[i];
-         int j;
-         for (j=i-1; j >= 0 && vector[j] > aux; j--){
-              vector[j+1] = vector[j];
-          }
-         vector[j+1] = aux;
+    
+    
+  public static void main(String[] args) {
+      Insertion insertion = new Insertion();
+      //crea la cadena
+      int[] nums = {5, 8, 69, 9, 33, 65, 45, 19, 43, 62, 71};
+      //lamamos el método que ordena
+      int[] insertion = insertion.Insercion(nums);
+      //recorremos el array
+      for (int i = 0; i < ordenarInsercion.length; i++) {
+          //Imprimimos la cadena
+          System.out.println(ordenarInsercion[i]);
       }
-   }
+  }
+   public int[] ordenarInsercion(int[] array) {
+        int aux; //Se crea un variable para usar temporalmente
+        //Utilizamos un for para rcorrer el tamaño de la matriz
+        for (int i = 1; i < array.length; i++) {
+            //la variable temporal toma el valor
+            aux = array[i];
+            //recorremos con otro for para ubicar si el temporal es mayor que los otrs números
+            for (int j = i - 1; j >= 0 && array[j] > aux; j--) {
+                array[j + 1] = array[j];
+                
+                array[j] = aux;
+            }
+        }
+        //retorna el array
+        return array;
+    }
 }
