6ff45c0dc4561196b6e4c3963d0087115695becb
edwin1993 <edwinapol12@hotmail.com>
2018-11-19 19:20:24 -0500

    Update news.service.ts
    comentarios

--- Edwin F. Avila/WebService/ws/src/app/news.service.ts
+++ Edwin F. Avila/WebService/ws/src/app/news.service.ts
@@ -1,16 +1,18 @@
+//librerias
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
 })
+//
 export class NewsService {
-
+//urls servicios solo get
 	COL_Noticias = "https://newsapi.org/v2/top-headlines?country=co&language=es&apiKey=890ed3b022e44dedaae9d79d12a03426";
 	ESP_Noticias = "http://www.rtve.es/api/noticias.json";
 
   constructor( private http: HttpClient ) { }
-
+//funcion para obtener la data del servicio mediante el get
   obtenerNoticias(){
   	return this.http.get( this.ESP_Noticias );
   }
