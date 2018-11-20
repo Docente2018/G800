bca442c525f9204d34ea5ce296593c53988aa091
edwin1993 <edwinapol12@hotmail.com>
2018-11-19 19:21:39 -0500

    Update app.component.ts
    comentarios

--- Edwin F. Avila/WebService/ws/src/app/app.component.ts
+++ Edwin F. Avila/WebService/ws/src/app/app.component.ts
@@ -8,11 +8,12 @@ import { NewsService } from '../app/news.service';
 })
 export class AppComponent {
   title = 'Noticiario';
-
+//Noticias
   noticias: any; 
   constructor( private newsService: NewsService ){
   	newsService.obtenerNoticias().subscribe(
   		val => {
+        //coje toda la data y la guarda en noticias
   			this.noticias = val.page.items;
   			console.log( this.noticias );  			
   		}
